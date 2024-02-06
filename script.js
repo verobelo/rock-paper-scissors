83641528d17640ebede5ae4fb0e9b059cd5fee92
verobelo <142001535+verobelo@users.noreply.github.com>
2024-02-06 19:47:54 +0100

    Create variables

---
+++ script.js
@@ -0,0 +1,9 @@
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
+let playerScore = 0;
+let computerScore = 0;
