dec28425465631508a22062dbbfee2ec3bc02c60
verobelo <142001535+verobelo@users.noreply.github.com>
2024-02-06 19:49:55 +0100

    Add function to play the game until the user or the computer reaches 5 points

--- script.js
+++ script.js
@@ -7,3 +7,43 @@ const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 
 let playerScore = 0;
 let computerScore = 0;
+
+function playRound(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = "It's a tie!";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    result = "You win!";
+    playerScore++;
+  } else {
+    result = "You lose!";
+    computerScore++;
+  }
+
+  playerDisplay.innerHTML = `PLAYER: ${playerChoice}`;
+  computerDisplay.innerHTML = `COMPUTER: ${computerChoice}`;
+  resultDisplay.innerHTML = `${result.toUpperCase()}`;
+  playerScoreDisplay.innerHTML = `${playerScore}`;
+  computerScoreDisplay.innerHTML = `${computerScore}`;
+
+  if (playerScore === 5) {
+    resultDisplay.classList.add("green");
+    resultDisplay.innerHTML = "CONGRATULATIONS! YOU WON!";
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+  }
+  if (computerScore === 5) {
+    resultDisplay.classList.add("red");
+    resultDisplay.innerHTML = "BAD LUCK! YOU LOST!";
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+  }
+}
